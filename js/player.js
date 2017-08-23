47f6ce3275255ddee87c9b524105c0108fd6caf8
tonykung <tonykung06@gmail.com>
2017-08-23 19:27:15 +0800

    requirejs loads modules written in AMD syntax, systemjs to load commonjs modules

---
+++ js/player.js
@@ -0,0 +1,18 @@
+// private members
+var playerName = '';
+
+function logPlayer() {
+    console.log('The current player is ' + playerName + '.');
+}
+
+function setName(newName) {
+    playerName = newName;
+}
+
+function getName() {
+    return playerName;
+}
+
+exports.logPlayer = logPlayer;
+exports.setName = setName;
+exports.getName = getName;
